7cac00b65243b4fcc82eb0c1d406c2fec162f0db
bugbit <ohernandezba@gmail.com>
2024-05-16 22:47:57 +0200

    Detectando el sistema táctil (Positions Detections)

---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private InputAction touchPosition;
+    private InputAction touchPress;
+    [SerializeField] private GameObject player;
+
+    void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        touchPosition = playerInput.actions["TouchPosition"];
+        touchPress = playerInput.actions["TouchPress"];
+    }
+
+    void OnEnable()
+    {
+        touchPress.performed += TouchPress;
+    }
+
+    void OnDisable()
+    {
+        touchPress.performed -= TouchPress;
+    }
+
+    private void TouchPress(InputAction.CallbackContext context)
+    {
+        // bool isPressed = context.ReadValueAsButton();
+
+        // Debug.Log($"Touch Pressed is: {isPressed}");
+
+        // Vector2 position = touchPosition.ReadValue<Vector2>();
+        // Debug.Log($"Touch Position is: {position}");
+
+        Vector3 position = Camera.main.ScreenToWorldPoint(touchPosition.ReadValue<Vector2>());
+
+        position.z = player.transform.position.z;
+        player.transform.position = position;
+    }
+}
