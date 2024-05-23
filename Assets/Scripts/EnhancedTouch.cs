4702654c6f7e390a6a55c530cd6d36aa1da1041f
Oscar Devs <ohernandezba@gmail.com>
2024-05-23 23:54:57 +0200

    Enhanced Touch

--- Assets/Scripts/EnhancedTouch.cs
+++ Assets/Scripts/EnhancedTouch.cs
@@ -1,18 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
 
 public class EnhancedTouch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInput playerInput;
+    private InputActionMap touchBasicMap;
+    private InputAction touchPosition;
+    private InputAction touchPress;
+    [SerializeField] private GameObject player;
+
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        //playerInput = GetComponent<PlayerInput>();
+        //touchBasicMap = playerInput.actions.FindActionMap("TouchBasic");
+        //touchPosition = touchBasicMap["TouchPosition"];
+        //touchPress = touchBasicMap["TouchPress"];
+    }
+
+    void OnEnable()
+    {
+        //touchPress.performed += TouchPress;
+        EnhancedTouchSupport.Enable();
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown += FingerDown;
+        TouchSimulation.Enable();
+    }
+
+    void OnDisable()
+    {
+        //touchPress.performed -= TouchPress;
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown -= FingerDown;
+        TouchSimulation.Disable();
+        EnhancedTouchSupport.Disable();
+    }
+
+    private void FingerDown(Finger finger)
+    {
+        bool isPressed = finger.isActive;
+
+        Debug.Log($"Touch Pressed is: {isPressed}");
+    }
+
+    private void TouchPress(InputAction.CallbackContext context)
+    {
+        // bool isPressed = context.ReadValueAsButton();
+
+        // Debug.Log($"Touch Pressed is: {isPressed}");
+
+        // Vector2 position = touchPosition.ReadValue<Vector2>();
+        // Debug.Log($"Touch Position is: {position}");
+
+        Vector3 position = Camera.main.ScreenToWorldPoint(touchPosition.ReadValue<Vector2>());
+
+        position.z = player.transform.position.z;
+        player.transform.position = position;
     }
 }
