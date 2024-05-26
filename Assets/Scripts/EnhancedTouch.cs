5da32f55cec28b1c2b5e0861d0cc5dbab4c6bd8b
bugbit <ohernandezba@gmail.com>
2024-05-26 17:04:39 +0200

    Enhanced Touch

--- Assets/Scripts/EnhancedTouch.cs
+++ Assets/Scripts/EnhancedTouch.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
 
 public class EnhancedTouch : MonoBehaviour
 {
@@ -28,18 +30,52 @@ public class EnhancedTouch : MonoBehaviour
 
     void OnEnable()
     {
-        //touchPress.performed += TouchPress;
         EnhancedTouchSupport.Enable();
-        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown += FingerDown;
+        //Touch.onFingerDown += FingerDown;
         TouchSimulation.Enable();
     }
 
     void OnDisable()
     {
         //touchPress.performed -= TouchPress;
-        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown -= FingerDown;
+        //Touch.onFingerDown -= FingerDown;
         TouchSimulation.Disable();
         EnhancedTouchSupport.Disable();
+    }
+
+    void Update()
+    {
+        //Debug.Log(Touch.activeTouches);
+
+        // foreach (var touch in Touch.activeTouches)
+        // {
+        //     switch (touch.phase)
+        //     {
+        //         case TouchPhase.None:
+        //             Debug.Log("No hay actividad");
+        //             break;
+        //         case TouchPhase.Began:
+        //             Debug.Log("Acabamos de tocar la pantalla con un dedo, hacer algo");
+        //             break;
+        //         case TouchPhase.Moved:
+        //             Debug.Log("Tenemos un touch en movimiento, posicion: " + touch.screenPosition);
+        //             break;
+        //         case TouchPhase.Ended:
+        //             Debug.Log("El toque continuo ha acabado en la posicion: " + touch.screenPosition);
+        //             break;
+        //         case TouchPhase.Stationary:
+        //             Debug.Log("Se está mantenido el toque sin movimiento");
+        //             break;
+        //         case TouchPhase.Canceled:
+        //             Debug.Log("No se llamará esta fase mediante la interacción del usuario");
+        //             break;
+        //     }
+        // }
+
+        var activeTouches = Touch.activeTouches;
+
+        for (var i = 0; i < activeTouches.Count; ++i)
+            Debug.Log("Active touch: " + activeTouches[i]);
     }
 
     private void FingerDown(Finger finger)
@@ -47,18 +83,11 @@ public class EnhancedTouch : MonoBehaviour
         bool isPressed = finger.isActive;
 
         Debug.Log($"Touch Pressed is: {isPressed}");
-    }
 
-    private void TouchPress(InputAction.CallbackContext context)
-    {
-        // bool isPressed = context.ReadValueAsButton();
+        Vector2 fingerPosition = finger.screenPosition;
+        Debug.Log($"Touch Position is: {fingerPosition}");
 
-        // Debug.Log($"Touch Pressed is: {isPressed}");
-
-        // Vector2 position = touchPosition.ReadValue<Vector2>();
-        // Debug.Log($"Touch Position is: {position}");
-
-        Vector3 position = Camera.main.ScreenToWorldPoint(touchPosition.ReadValue<Vector2>());
+        Vector3 position = Camera.main.ScreenToWorldPoint(fingerPosition);
 
         position.z = player.transform.position.z;
         player.transform.position = position;
